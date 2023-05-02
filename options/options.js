f8d74632a98b163da7be5dea4bfdd02fb50824c2
Crowdbotics <team@crowdbotics.com>
2023-05-02 08:57:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "digitalcard-40395",
+  url: "https://digitalcard-40395.botics.co",
+  api: "https://digitalcard-40395.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
